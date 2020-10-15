5bbfe3a40c22fc612586c0d905f8a12898e11486
Tiago Esperança Triques <tiago.triques@gmail.com>
2020-10-15 09:13:11 -0300

    Add files via upload

--- t2.c
+++ t2.c
@@ -51,7 +51,7 @@ double calculate_median(double *v, int n){
 
 double calculate_arithmetic_mean(double *v, int n) {
 	double sum = 0.0;
-	
+
 	for(int i = 0; i < n; i++)
 		sum += v[i];
 
@@ -60,7 +60,7 @@ double calculate_arithmetic_mean(double *v, int n) {
 
 double calculate_harmonic_mean(double *v, int n) {
 	double sum = 0.0;
-	
+
 	for(int i = 0; i < n; i++)
 		sum += 1.0/v[i];
 
@@ -77,20 +77,19 @@ double calculate_variance(double *v, int n, double avg) {
 }
 
 int main(int argc, char *argv[]) {
-	int a;
 	int i, j;
-    double **matrix = NULL;
+	double **matrix = NULL;
 	int rows, cols;
 	rows = cols = 0;
 
-	a=scanf("%d %d", &rows, &cols);
+	scanf("%d %d", &rows, &cols);
 
 	// Recebendo a matriz e já armazenando a transposta
 	// para facilitar depois :)
 	matrix = (double **) calloc(cols, sizeof(double *));
 	for(i = 0; i < cols; matrix[i++] = (double *) calloc(rows, sizeof(double)));
 	for(i = 0, j = 0; i < rows; i++)
-		for(j = 0; j < cols; a=scanf("%lf", &matrix[j++][i]));
+		for(j = 0; j < cols; scanf("%lf", &matrix[j++][i]));
 
 	// Como estamos trabalhando com a transposta
 	// fazemos o swap de rows e cols
@@ -99,34 +98,35 @@ int main(int argc, char *argv[]) {
 	//Vetor de struct para armazenar as métricas
 	Metrics *metrics = (Metrics *) calloc(rows, sizeof(Metrics));
 
+
 	int x[rows];
 
 	double wtime = omp_get_wtime();
 
-    #pragma omp parallel num_threads(8) private(i) shared(metrics, matrix)
-    {
-        #pragma omp single
-        {
-            for(i = 0; i < rows; i++) {
-                #pragma omp task depend(out: x[i])
-                metrics[i].median = calculate_median(matrix[i], cols);
-                
-				#pragma omp task depend(in: x[i])
-                {
-                    metrics[i].arith_mean = calculate_arithmetic_mean(matrix[i], cols);
-                    metrics[i].variance = calculate_variance(matrix[i], cols, metrics[i].arith_mean);
-                    metrics[i].std_dev = sqrt(metrics[i].variance);
-                    metrics[i].coef  = metrics[i].std_dev/metrics[i].arith_mean;
-                }
+	#pragma omp parallel num_threads(8) private(i) shared(metrics, matrix)
+	{
+		#pragma omp single
+		{
+			for(i = 0; i < rows; i++) {
+				#pragma omp task depend(out: x[i])
+				metrics[i].median = calculate_median(matrix[i], cols);
 
-                #pragma omp task depend(in: x[i])
-                metrics[i].harm_mean = calculate_harmonic_mean(matrix[i], cols);
-                
-                #pragma omp task depend(in: x[i])
+				#pragma omp task depend(in: x[i])
+				{
+					metrics[i].arith_mean = calculate_arithmetic_mean(matrix[i], cols);
+					metrics[i].variance = calculate_variance(matrix[i], cols, metrics[i].arith_mean);
+					metrics[i].std_dev = sqrt(metrics[i].variance);
+					metrics[i].coef  = metrics[i].std_dev/metrics[i].arith_mean;
+				}
+
+				#pragma omp task depend(in: x[i])
+				metrics[i].harm_mean = calculate_harmonic_mean(matrix[i], cols);
+
+				#pragma omp task depend(in: x[i])
 				metrics[i].mode = calculate_mode(matrix[i], cols);
-            }
-        }
-    }
+			}
+		}
+	}
 
 	wtime = omp_get_wtime() - wtime;
 
@@ -166,5 +166,5 @@ int main(int argc, char *argv[]) {
 	free(matrix);
 	free(metrics);
 
-    return 0;
+	return 0;
 }
